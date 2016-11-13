05a7c701ca256795f147b4994d8825937d79cf77
tarpisu <takumi.hanzawa131@gmail.com>
2016-11-14 02:24:27 +0900

    add

---
+++ Assets/Example/Example.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Example : MonoBehaviour {
+
+	[SerializeField]
+	PlayerData playerData;
+
+	public void OnClickSave(){
+		LocalDataSaver.Save<PlayerData>(playerData);
+	}
+
+	public void OnClickLoad(){
+		playerData = LocalDataSaver.Load<PlayerData>();
+	}
+
+	public void OnClickDelete(){
+		LocalDataSaver.Delete<PlayerData>();
+	}
+}
+
+[System.Serializable]
+public class PlayerData{
+	public string name;
+	public int hp;
+	public int mp;
+}
